92c4a3b6b8dff35d75bcc327b2d9381d9f1085b8
Matt Beal <mrbeal15@gmail.com>
2019-03-13 14:23:48 -0500

    add methods to tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,14 @@
 class Tweet < ApplicationRecord
+  scope :by_date, -> { order(published_date: :desc).take(10)}
+
+  def self.retrieve_recent_tweets(topic)
+    response_body = HTTP.headers(
+        :Authorization => "bearer #{ENV['TWITTER_TOKEN']}"
+      ).get("#{ENV['TWITTER_HOST']}/search/tweets.json?q=#{topic}&result_type=recent").body
+
+    result = JSON.parse(response_body)
+    result['statuses'].each do |tweet|
+        Tweet.first_or_create(topic: topic, author: tweet['user']['screen_name'], content: tweet['text'], published_date: tweet['created_at'])
+    end
+  end
 end
